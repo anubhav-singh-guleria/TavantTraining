2112a222fcd54ff76e819d28526efc55c98811cf
asura69 <anubhavsguleria@gmail.com>
2022-08-10 09:20:03 +0530

    8th aug

---
+++ August/8Aug/mockito/src/main/java/com/unittesting/mockito/PatientRecord.java
@@ -0,0 +1,34 @@
+package com.unittesting.mockito;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Table(name = "patient_record")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PatientRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long patientId;
+    
+    @NonNull
+    private String name;
+ 
+    @NonNull
+    private Integer age;
+    
+    @NonNull 
+    private String address;
+}
---
+++ August/8Aug/mockito/src/main/java/com/unittesting/mockito/PatientRecordRepository.java
@@ -0,0 +1,9 @@
+package com.unittesting.mockito;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long> {
+    
+}
