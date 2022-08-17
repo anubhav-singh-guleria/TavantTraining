f1f32a9b318184cb08f486ba96a042b28e3ddb02
asura69 <anubhavsguleria@gmail.com>
2022-08-17 08:33:15 +0530

    august all

--- August/8Aug/mockito/src/main/java/com/unittesting/mockito/PatientRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unittesting.mockito;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long> {
-    
-}
